39cc11dd63a7584c777814028e8234499970908d
Alberto696969 <71942953+albertoverrilli@users.noreply.github.com>
2024-08-05 23:56:13 -0400

    Update BackupOperaGXBookmarks.ps1

--- BackupOperaGXBookmarks.ps1
+++ BackupOperaGXBookmarks.ps1
@@ -1,13 +1,13 @@
 # Define the source path for Opera GX bookmarks
-$sourcePath = "C:\Users\alber\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
+$sourcePath = "C:\Users\Your_Name\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
 
 # Define the destination path for the backup
-$destinationPath = "C:\Users\alber\Documents\My Notebook\The AlgorIthm\Knowledge\Personal\Bookmarks\Bookmarks_Backup.html"
+$destinationPath = "C:\Users\...\Bookmarks_Backup.html"
 
 # Copy the bookmarks file to the backup location
 Copy-Item -Path $sourcePath -Destination $destinationPath -Force
 
 
 # Run the Python script to convert the HTML bookmarks to Markdown
-$pythonScriptPath = "C:\Users\alber\Documents\My Notebook\The AlgorIthm\Knowledge\Personal\Bookmarks\Convert_Bookmarks.py"
+$pythonScriptPath = "C:\Users\...\Convert_Bookmarks.py"
 python $pythonScriptPath
